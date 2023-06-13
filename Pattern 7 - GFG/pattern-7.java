039a9aae5bb1d413928ca9289908f69c80c46063
satyam1196 <52503795+satyam1196@users.noreply.github.com>
2023-06-13 22:26:13 +0530

    Added solution - LeetHub

---
+++ Pattern 7 - GFG/pattern-7.java
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Main {
+    // Driver code
+    public static void main(String[] args) throws Exception {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t-- > 0) {
+            int n = Integer.parseInt(br.readLine().trim());
+            Solution obj = new Solution();
+            obj.printTriangle(n);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+
+    void printTriangle(int N) {
+        // code here
+        for(int i=0;i<N;i++){
+            // for(int j=0;i<n-i-1;j++){
+            //     System.out.print(" ");
+            // }
+            
+            // for(int j=0;j<2*i+1;j++){
+            //     System.out.print("*");
+            // }
+            // //  for(int j=0;i<n-i-1;j++){
+            // //     System.out.print(" ");
+            // // }
+                    for (int j =0; j<N-i-1; j++)
+        {
+            System.out.print(" ");
+        }
+       
+        // For printing the stars in each row
+        for(int j=0;j< 2*i+1;j++){
+            
+            System.out.print("*");
+        }
+        
+        // // For printing the spaces after the stars in each row
+        //  for (int j =0; j<N-i-1; j++)
+        // {
+        //     System.out.print(" ");
+        // }
+            
+            System.out.println();
+            
+        }
+        
+    }
+}
