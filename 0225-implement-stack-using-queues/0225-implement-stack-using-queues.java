669548a7cb3d367758fcc4af4d5773ea892188ae
satyam1196 <52503795+satyam1196@users.noreply.github.com>
2023-08-29 12:39:36 +0530

    Time: 0 ms (100.00%), Space: 39.9 MB (98.13%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
@@ -1,28 +1,34 @@
 class MyStack {
     private  Queue<Integer> q1;
-    private  Queue<Integer> q2;
+    //private  Queue<Integer> q2;
 
     public MyStack() {
      q1=new LinkedList<>();
-     q2=new LinkedList<>();
+     //q2=new LinkedList<>();
     }
     
     public void push(int x) {      
-        q2.add(x);
-        while(!q1.isEmpty()){
-            q2.add(q1.peek());
-            q1.remove();
-        }
+//         q2.add(x);
+//         while(!q1.isEmpty()){
+//             q2.add(q1.peek());
+//             q1.remove();
+//         }
         
-        swap();
+//         swap();
+        
+        q1.add(x);
+        
+        for(int i=0;i<q1.size()-1;i++){
+            q1.add(q1.remove());
+        }
         
     }
     
-    private void swap() {
-        Queue<Integer> temp = q1;
-        q1 = q2;
-        q2 = temp;
-    }
+    // private void swap() {
+    //     Queue<Integer> temp = q1;
+    //     q1 = q2;
+    //     q2 = temp;
+    // }
     
     public int pop() {
         return q1.remove();
