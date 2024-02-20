6a36965f246966750606240221e906713357dc8f
satyam1196 <52503795+satyam1196@users.noreply.github.com>
2024-02-20 23:47:52 +0530

    Time: 0 ms (100.00%), Space: 45.6 MB (16.90%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,13 +1,12 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        Arrays.sort(nums);
+        int sum=0;
+        int n=nums.length;
         
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]!=i){
-                return i;
-            }
+        for(int num:nums){
+            sum+=num;
         }
         
-        return nums.length;
+        return n*(n+1)/2 -sum;
     }
 }
