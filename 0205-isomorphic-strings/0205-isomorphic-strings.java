690d2ed163ded23ef192d48feb74255a64c6ef4f
satyam1196 <52503795+satyam1196@users.noreply.github.com>
2024-04-03 01:09:19 +0530

    Time: 25 ms (9.50%), Space: 42.2 MB (66.94%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,28 +1,37 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        
-        if(s.length()!=t.length()){
-            return false;
-        }
-        Map<Character,Character> map1=new HashMap<>();
-        Map<Character,Character> map2=new HashMap<>();
-        for(int i=0;i<s.length();i++){
-            if(map1.containsKey(s.charAt(i))){
-                if(map1.get(s.charAt(i))!=t.charAt(i)){
-                    return false;
-                }
+        Map<Character,Character> mp1=new HashMap<>();
+        Map<Character,Character> mp2=new HashMap<>();
+        // for(int i=0;i<s.length();i++){
+        //     if(map1.containsKey(s.charAt(i))){
+        //         if(map1.get(s.charAt(i))!=t.charAt(i)){
+        //             return false;
+        //         }
+        //     }
+        //     else{
+        //         map1.put(s.charAt(i),t.charAt(i));
+        //     }
+        //     if(map2.containsKey(t.charAt(i))){
+        //         if(map2.get(t.charAt(i))!=s.charAt(i)){
+        //             return false;
+        //         }
+        //     }
+        //     else{
+        //         map2.put(t.charAt(i),s.charAt(i));
+        //     }
+        // }
+         for(int i = 0; i < s.length(); i++) {
+            
+            char ch1 = s.charAt(i);
+            char ch2 = t.charAt(i);
+            
+            if((mp1.containsKey(ch1) && mp1.get(ch1) != ch2) || 
+               (mp2.containsKey(ch2) && mp2.get(ch2) != ch1)) {
+                return false;
             }
-            else{
-                map1.put(s.charAt(i),t.charAt(i));
-            }
-            if(map2.containsKey(t.charAt(i))){
-                if(map2.get(t.charAt(i))!=s.charAt(i)){
-                    return false;
-                }
-            }
-            else{
-                map2.put(t.charAt(i),s.charAt(i));
-            }
+            
+            mp1.put(ch1, ch2);
+            mp2.put(ch2, ch1);
         }
         return true;
     }
