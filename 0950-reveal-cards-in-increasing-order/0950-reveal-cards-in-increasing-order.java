1a522cc05007479a924965ed635627aa781b72e1
satyam1196 <52503795+satyam1196@users.noreply.github.com>
2024-04-10 21:49:30 +0530

    Time: 4 ms (86.41%), Space: 43.5 MB (52.91%) - LeetHub

--- 0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.java
+++ 0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.java
@@ -1,25 +1,47 @@
 class Solution {
     public int[] deckRevealedIncreasing(int[] deck) {
-        Arrays.sort(deck);
-        int n=deck.length;
-        int[] result=new int[n];
-        Arrays.fill(result,0);
+        //CodeStoryWithMick //Approach 1
+//         Arrays.sort(deck);
+//         int n=deck.length;
+//         int[] result=new int[n];
+//         Arrays.fill(result,0);
         
-        int i=0;
-        int j=0;
-        boolean skip=false;
+//         int i=0;
+//         int j=0;
+//         boolean skip=false;
         
-        while(i<n){
+//         while(i<n){
             
-            if(result[j]==0){
-                if(!skip){
-                result[j]=deck[i];
-                i++;
-                }
-                 skip=!skip; 
+//             if(result[j]==0){
+//                 if(!skip){
+//                 result[j]=deck[i];
+//                 i++;
+//                 }
+//                  skip=!skip; 
+//             }
+//             j=(j+1)%n;  
+//         }   
+//         return result;
+        
+        
+        //Approach 2
+        
+        Arrays.sort(deck);
+        Queue<Integer> queue=new LinkedList<>();
+        int n=deck.length;
+        for(int i=0;i<n;i++){
+            queue.add(i);
+        }
+        int[] result=new int[n];
+        for(int i=0;i<n;i++){
+            int index=queue.poll();
+            result[index]=deck[i];
+            
+            if(!queue.isEmpty()){
+                queue.add(queue.peek());
+                queue.poll();
             }
-            j=(j+1)%n;  
-        }   
+        }
         return result;
     }
 }
